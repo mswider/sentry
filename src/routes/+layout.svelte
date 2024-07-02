0cd8954edf70dbedb847ae7059c2f8e1c0be1ee6
mswider <matthew.swider@outlook.com>
2024-07-02 15:38:55 -0500

    feat: add web app basics

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang='ts'>
+    import "../app.css"
+    import logo from "../lib/assets/screen-share.svg"
+
+    function handleMediaQuery() {
+		document.body.parentElement!.dataset.theme = window.matchMedia('(prefers-color-scheme: dark)').matches
+			? 'forest'
+			: 'nord';
+	}
+
+	if (window.matchMedia) {
+		handleMediaQuery();
+		window
+			.matchMedia('(prefers-color-scheme: dark)')
+			.addEventListener('change', handleMediaQuery);
+	}
+</script>
+
+<nav class="navbar bg-base-300">
+    <div class="flex-none gap-x-4">
+        <img src="{logo}" alt="logo" class="dark:invert">
+        <h1 class="text-3xl font-bold">Sentry</h1>
+    </div>
+</nav>
+<slot></slot>
