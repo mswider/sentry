30082b4a07143a9b416d6614c26a31675219c477
mswider <matthew.swider@outlook.com>
2024-07-10 15:16:46 -0500

    feat: add script to build config

---
+++ lib/buildHelper.ts
@@ -0,0 +1,14 @@
+import * as path from 'node:path'
+import * as fs from 'node:fs/promises'
+import config from '../sentry.config.ts'
+
+const configOutputPath = path.join(path.dirname(import.meta.dirname), 'static', 'config.json')
+const configOutput = JSON.stringify(config)
+
+try {
+    await fs.writeFile(configOutputPath, configOutput)
+    console.log('Saved')
+} catch (error) {
+    console.error('Failed to write config file:', error)
+    process.exitCode = 1
+}
