bec30bd34d14dc0f0f017d72cf0491e670ab3f76
mswider <matthew.swider@outlook.com>
2024-07-15 18:42:22 -0500

    feat: add generic error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<main class="m-4">
+    <h1 class="text-3xl font-bold mb-1">Error</h1>
+    <h2>{$page.status}: {$page.error?.message || '(no details)'}</h2>
+</main>
