08f43c1de5ac7bd432b6e84a59d8345e2dd6887e
mswider <matthew.swider@outlook.com>
2024-09-28 15:34:43 -0500

    feat: convert to Worker

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 import * as child_process from 'node:child_process';
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare-workers';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({ fallback: '404.html', precompress: false }),
+		adapter: adapter({
+			config: 'wrangler.toml',
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				persist: false
+			}
+		}),
 		version: {
 			name: child_process.execSync('git describe --tags --abbrev=0').toString().trim()
 		},
